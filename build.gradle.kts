dac53dc90ab74ada9dff3f64e955b1f0c12da641
Mihael Berčič <mihael.register@gmail.com>
2021-01-02 23:56:47 +0100

    Removed two dependancies, that were far from needed.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,5 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.slf4j:slf4j-simple:2.0.0-alpha1")
-    implementation("commons-codec:commons-codec:1.14")
     implementation("io.javalin:javalin:3.12.0")
-    implementation("com.nimbusds:srp6a:2.1.0")
 }
--- src/main/kotlin/homekit/HomeKitService.kt
+++ src/main/kotlin/homekit/HomeKitService.kt
@@ -1,14 +1,18 @@
 package homekit
 
 import generateRandomMAC
-import mdns.*
+import hash
+import mdns.Header
+import mdns.MulticastService
+import mdns.Packet
+import mdns.asDatagramPacket
 import mdns.records.ARecord
 import mdns.records.PTRRecord
 import mdns.records.SRVRecord
 import mdns.records.TXTRecord
-import org.apache.commons.codec.digest.DigestUtils
 import java.net.InetAddress
 import java.net.MulticastSocket
+import java.security.MessageDigest
 import java.util.*
 
 /**
@@ -19,6 +23,7 @@ import java.util.*
 class HomeKitService(name: String = "HomeKit") : MulticastService("_hap._tcp.local", InetAddress.getLocalHost()) {
 
     private val recordName = "$name.$protocol"
+    private val digest = MessageDigest.getInstance("SHA-512")
     private val myMAC = generateRandomMAC() // TODO TAKE PERSISTENT FROM JSON
 
     override var onDiscovery: MulticastSocket.() -> Unit = {
@@ -44,7 +49,7 @@ class HomeKitService(name: String = "HomeKit") : MulticastService("_hap._tcp.loc
         "ci"..1
         "ff"..0
         "md"..name
-        "sh"..Base64.getEncoder().encodeToString(DigestUtils.sha512("1$myMAC"))
+        "sh"..Base64.getEncoder().encodeToString(digest.hash(*"1$myMAC".toByteArray()))
     }
 
     private val discoveryPacket = Packet(Header(isResponse = true)).apply {
