5755e48ac927dfca3db2bd163f2728a9eb79b49b
Mihael Berčič <mihael@regnum.si>
2022-05-21 00:02:31 +0200

    Added retrying attempts for reboots where network is not yet setup-.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,17 @@
 import homekit.HomeKitServer
 import homekit.Settings
+import utils.Logger
 import utils.generateMAC
 import utils.readOrCompute
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 fun main() {
+    startHomeKit()
+}
+
+fun startHomeKit() {
+    Logger.info("Attempting to run HomeKit.")
     try {
         val settings = readOrCompute("settings.json") {
             Settings(1, 3000, generateMAC())
@@ -11,5 +19,7 @@ fun main() {
         HomeKitServer(settings).start()
     } catch (e: Exception) {
         e.printStackTrace()
+        Logger.info("Retrying in 5 seconds...")
+        Timer().schedule(5000) { startHomeKit() }
     }
 }
